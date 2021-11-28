9f9fd8cc96d9f37db7e99946d35dbf84cb6ec14c
jeremyasirwaad <71336703+jeremyasirwaad@users.noreply.github.com>
2021-11-28 13:48:23 +0530

    Updated Header

--- src/components2/Header.js
+++ src/components2/Header.js
@@ -16,7 +16,7 @@ function Home() {
                     <Link to = "/"><div class="nav-logo">Gct Intership Portal</div></Link>
                     <ul class={ "nav-menu"  + ( clicks ? ( " active" ) : ( "" ) ) }>
                         <li class="nav-item">
-                            <Link to = "/posts"><div class="nav-link">Intership Posts</div></Link>
+                            <Link to = "/posts"><div class="nav-link">Internship Posts</div></Link>
                             {/* <Link to = "/register">Register</Link> */}
                         </li>
                         <li class="nav-item">
